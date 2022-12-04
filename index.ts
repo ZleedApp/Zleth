c356a3cedc0e200508d65244f1cb32a6a3482a0f
TheClashFruit <clash.fruit123@gmail.com>
2022-12-04 12:26:02 +0100

    feat: setup express

--- index.ts
+++ index.ts
@@ -1,18 +1,28 @@
+import dotenv     from 'dotenv'
 import express    from 'express';
 import cors       from 'cors';
 import bodyparser from 'body-parser';
 import fs         from 'fs';
 import path       from 'path';
 
+dotenv.config();
+
 const app = express();
 
-app.use(bodyparser.json());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
+
+fs.readdirSync(path.join(__dirname, 'endpoints', 'v1'))
+    .filter(file => file.endsWith('.ts'))
+    .forEach(endpointFile =>
+        require(path.join(__dirname, 'endpoints', 'v1', endpointFile)).addEndpoint(app)
+    );
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port 3000');
+app.listen(process.env.API_PORT, () => {
+    console.log(`Listening on 0.0.0.0:${process.env.API_PORT}`);
 });
