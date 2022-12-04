a25c3216992c79f12ab6ee3d3e54ea85eaf4b0c7
TheClashFruit <clash.fruit123@gmail.com>
2022-12-04 12:47:45 +0100

    feat: add ESLint

--- endpoints/v1/auth.ts
+++ endpoints/v1/auth.ts
@@ -1,5 +1,10 @@
-module.exports = {
-    addEndpoint: (app: any) => {
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
-    }
-}
+module.exports = {
+  addEndpoint: (app: any, mongoose: any) => {
+    app.post('/v1/auth/register', async (req: any, res: any) => {
+
+    });
+  }
+};
--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import dotenv     from 'dotenv'
+import dotenv     from 'dotenv';
 import express    from 'express';
 import cors       from 'cors';
 import bodyparser from 'body-parser';
 import fs         from 'fs';
 import path       from 'path';
+import mongoose   from 'mongoose';
 
 dotenv.config();
 
@@ -13,16 +14,19 @@ app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
 
+mongoose.connect(process.env.DB_URI!)
+  .then(() => console.log('Connected to database!'));
+
 fs.readdirSync(path.join(__dirname, 'endpoints', 'v1'))
-    .filter(file => file.endsWith('.ts'))
-    .forEach(endpointFile =>
-        require(path.join(__dirname, 'endpoints', 'v1', endpointFile)).addEndpoint(app)
-    );
+  .filter(file => file.endsWith('.ts'))
+  .forEach(endpointFile =>
+    require(path.join(__dirname, 'endpoints', 'v1', endpointFile)).addEndpoint(app, mongoose)
+  );
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(process.env.API_PORT, () => {
-    console.log(`Listening on 0.0.0.0:${process.env.API_PORT}`);
+  console.log(`Listening on 0.0.0.0:${process.env.API_PORT}`);
 });
